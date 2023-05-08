c30d6a45f50b319908cbdbb0b45d527ebda38b38
Si1adan <mark.pyankov@mail.ru>
2023-05-08 20:43:48 +0600

    step two

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,4 +6,6 @@ program
   .name('gendiff')
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>')
   .parse();
