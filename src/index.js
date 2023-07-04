0d919787bbb55932077f247296ce33b84ac40616
Si1adan <mark.pyankov@mail.ru>
2023-07-04 23:48:50 +0600

    fixes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-import pathResolveToObj from './pathResolver.js';
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
+import parse from './parsers.js';
 import genDiff from './genDiff.js';
 import format from './formatters/index.js';
+
+const pathResolveToObj = (p) => {
+  const path = resolve(`${p}`);
+
+  const file = readFileSync(path);
+
+  const parsedFile = parse(path, file);
+
+  return parsedFile;
+};
 
 const genTree = (path1, path2, formatName = 'stylish') => {
   const [obj1, obj2] = [pathResolveToObj(path1), pathResolveToObj(path2)];
