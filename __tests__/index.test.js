11967e39be32e057b9c5944d72eaf5079df20699
Si1adan <mark.pyankov@mail.ru>
2023-07-05 14:22:12 +0600

    fix tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -14,44 +14,36 @@ const [pathJSON1, pathJSON2, pathYML1, pathYML2] = [
 ]
   .map((name) => getFixturePath(name));
 
-const [expectedJSON, expectedPlain, expectedStylish] = [
-  'expected_file_JSON.yml', 'expected_file_plain.yml', 'expected_file_stylish.yml',
-]
-  .map((name) => readFile(name));
-
 const testData1 = [
   {
-    path1: pathJSON1, path2: pathJSON2, expected: expectedStylish, type: 'JS',
+    path1: pathJSON1, path2: pathJSON2, expected: readFile('expected_file_stylish.yml'), type: 'JS',
   },
   {
-    path1: pathYML1, path2: pathYML2, expected: expectedStylish, type: 'YML',
+    path1: pathYML1, path2: pathYML2, expected: readFile('expected_file_stylish.yml'), type: 'YML',
+  },
+  {
+    path1: pathYML1, path2: pathYML2, expected: readFile('expected_file_plain.yml'), type: 'plain',
+  },
+  {
+    path1: pathJSON1, path2: pathJSON2, expected: readFile('expected_file_JSON.yml'), type: 'json',
   },
 ];
 
-const testData2 = [
-  {
-    path1: pathYML1, path2: pathYML2, expected: expectedPlain, type: 'plain',
-  },
-  {
-    path1: pathJSON1, path2: pathJSON2, expected: expectedJSON, type: 'json',
-  },
-];
-
-const testData3 = {
+const testData2 = {
   path1: pathJSON1, path2: pathJSON2, expected: 'format txt is not supported', type: 'txt',
 };
 
 describe('genTree tests', () => {
-  test.each(testData1)('$type case', ({ path1, path2, expected }) => expect(genTree(path1, path2)).toBe(expected));
-
-  test.each(testData2)('$type case', ({
+  test.each(testData1)('$type case', ({
     path1, path2, expected, type,
-  }) => expect(genTree(path1, path2, type)).toBe(expected));
+  }) => (type === 'JS' || type === 'YML'
+    ? expect(genTree(path1, path2)).toBe(expected)
+    : expect(genTree(path1, path2, type)).toBe(expected)));
 
   test.failing('txt case', () => {
     const {
       path1, path2, expected, type,
-    } = testData3;
+    } = testData2;
     expect(genTree(path1, path2, type)).toThrow(Error, expected);
   });
 });
